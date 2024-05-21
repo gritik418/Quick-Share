2daae6d6cf74e71e10a589239216f0041de72f6e
Ritik <160255700+gritik418@users.noreply.github.com>
2024-05-21 21:27:32 +0530

    Redux toolkit integrated

---
+++ src/features/auth/authSlice.ts
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isLoggedIn: false,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {},
+});
+
+export default authSlice.reducer;

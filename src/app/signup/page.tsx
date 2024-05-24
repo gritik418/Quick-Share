2d2c5a5cb41b27f33b0a95e18d6318471292b090
Ritik <160255700+gritik418@users.noreply.github.com>
2024-05-24 13:59:24 +0530

    Sign up page ui completed

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -14,7 +14,7 @@ const SignUp = () => {
       <Navbar />
       <div className={styles.container}>
         <div className={styles.content}>
-          <h1 className={styles.heading}>SignUp</h1>
+          <h1 className={styles.heading}>Create a new account</h1>
           <p className={styles.title}>
             Already have an account?
             <Link href={"/login"} className={styles.link}>
@@ -22,6 +22,30 @@ const SignUp = () => {
               Login
             </Link>
           </p>
+
+          <label htmlFor="first_name" className={styles.label}>
+            First Name
+          </label>
+          <Input
+            className={`${styles.input} mb-8`}
+            name="first_name"
+            id="first_name"
+            pr="4.5rem"
+            type="text"
+            placeholder="Enter First Name"
+          />
+
+          <label htmlFor="last_name" className={styles.label}>
+            Last Name
+          </label>
+          <Input
+            className={`${styles.input} mb-8`}
+            name="last_name"
+            id="last_name"
+            pr="4.5rem"
+            type="text"
+            placeholder="Enter Last Name"
+          />
 
           <label htmlFor="email" className={styles.label}>
             Email Address
@@ -32,19 +56,19 @@ const SignUp = () => {
             id="email"
             pr="4.5rem"
             type="email"
-            placeholder="Enter email address"
+            placeholder="Enter Email Address"
           />
 
           <label htmlFor="password" className={styles.label}>
             Password
           </label>
-          <InputGroup size="md" className={styles.input}>
+          <InputGroup size="md" className={`${styles.input} mb-8`}>
             <Input
               name="password"
               id="password"
               pr="4.5rem"
               type={show ? "text" : "password"}
-              placeholder="Enter password"
+              placeholder="Enter Password"
             />
             <InputRightElement width="4.5rem">
               <Button h="1.75rem" size="sm" onClick={handleClick}>
@@ -53,10 +77,26 @@ const SignUp = () => {
             </InputRightElement>
           </InputGroup>
 
-          <div className={styles.forgot}>
-            <Link href={"/forgot-password"}> Forgot Password?</Link>
-          </div>
-          <button className={styles.btn}>SignUp</button>
+          <label htmlFor="confirm_password" className={styles.label}>
+            Confirm Password
+          </label>
+
+          <InputGroup size="md" className={styles.input}>
+            <Input
+              name="password_confirmation"
+              id="confirm_password"
+              pr="4.5rem"
+              type={show ? "text" : "password"}
+              placeholder="Enter Confirm Password"
+            />
+            <InputRightElement width="4.5rem">
+              <Button h="1.75rem" size="sm" onClick={handleClick}>
+                {show ? "Hide" : "Show"}
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+
+          <button className={styles.btn}>Sign Up</button>
         </div>
       </div>
     </>

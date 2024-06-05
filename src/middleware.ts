3ba7a8441515904f7b11bb3b67e2fd22439be8cc
Ritik <160255700+gritik418@users.noreply.github.com>
2024-06-05 18:20:22 +0530

    Middleware updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const token = request.cookies.get("token");
-  if (token?.value !== "" || token?.value !== null) {
+  if (token) {
     return NextResponse.redirect(new URL("/", request.url));
   }
 }
